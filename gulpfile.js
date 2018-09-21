434cef548b5023187eae6b1c172405036aabed6a
Muhammad Kalthoum <Moe@Muhammads-iMac.local>
2018-09-21 01:48:02 -0400

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,28 @@ var browserSync = require('browser-sync').create();
 // Gulp tasks begins here
 // Sass compiler
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('app/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+    return gulp.src("app/scss/main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
 });
 
-//Sync - Spin a server for ma
-gulp.task('broswer-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-});
 // Gulp watch
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('watch', ['serve', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
 
 
 //Sync - Spin a server for ma
-gulp.task('broswer-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "app"
+    });
+
+    gulp.watch("app/scss/**/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
+
+gulp.task('default', ['serve']);
